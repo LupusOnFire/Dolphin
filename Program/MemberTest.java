528adee7a560af0d8e5d930848f4a38e761cf35a
Bertram <bertram@lupusmedia.dk>
2016-11-23 13:37:31 +0100

    merge problemer

--- Program/MemberTest.java
+++ Program/MemberTest.java
@@ -1,7 +1,32 @@
+import java.io.*;
+import java.util.*;
 public class MemberTest {
    public static void main (String[] args)throws Exception {
-      Member m = new Member("280493-0003", "Bertram", "Baltzer Andersen", "88888888", true, 0.0, 0.0);
-      System.out.println(m.getAge());
-      System.out.println(m.getGender());
+      Locale.setDefault(new Locale("en", "US"));
+      ArrayList<Subscription> subscriptions = new ArrayList<>();
+      ArrayList<Member> members = new ArrayList<>();
+      Scanner subscriptionReader = new Scanner(new File("data/subscription.txt"));
+      while (subscriptionReader.hasNextLine()){
+         int id = subscriptionReader.nextInt();
+         String type = subscriptionReader.next();
+         double price = subscriptionReader.nextDouble();
+         Subscription s = new Subscription(id, type, price);
+         subscriptions.add(s);
+      }
+      Scanner memberReader = new Scanner(new File("data/member.txt"));
+      while (memberReader.hasNextLine()) {
+         Subscription sub = null;
+         String line = memberReader.nextLine();
+         String[] str = line.split(":");
+         for (Subscription s : subscriptions) {
+            if (s.getId() == Integer.parseInt(str[8])){
+               sub = s;
+               break;
+            }
+         }   
+         Member m = new Member(str[0], str[1], str[2], str[3], str[4], Boolean.parseBoolean(str[5]), Boolean.parseBoolean(str[6]), Double.parseDouble(str[7]), sub);
+         members.add(m);
+      }   
+      System.out.println(members);
    }
 }
