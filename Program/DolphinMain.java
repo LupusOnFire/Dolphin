f191d80188591fea66bc1ee4839343870c343629
Leivur Hammer <Leivur Hammer>
2016-11-29 10:44:21 +0100

    economy

--- Program/DolphinMain.java
+++ Program/DolphinMain.java
@@ -1,8 +1,16 @@
+import java.util.*;
+import java.io.*;
+
 public class DolphinMain {
     public static void main(String[] args) throws Exception {
         Engine e = new Engine();
+        Scanner console = new Scanner(System.in);
         e.loadData();
-        e.printMembers();
-        e.printRecords();
+        //e.printMembers();
+        //e.printRecords();
+        //e.printCredit();
+        e.totalIncome();
+        // Spørg om CPR i main, laver lige temporary
+        //e.editBalance("280493-0003", console);
     }
 }
