fc574006c44c062bc1c26331a3a947073b72a38f
Leivur Hammer <Leivur Hammer>
2016-12-07 12:28:02 +0100

    ups kom til at slette noget i show income, added det igen

--- Program/Menu.java
+++ Program/Menu.java
@@ -799,6 +799,7 @@ public class Menu {
       for (Member m : members) {
          income += m.getSubscription().getPrice();
       }
+      System.out.println(income);
       System.out.println();
    
    }
